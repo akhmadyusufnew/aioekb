ede670215efb8084d85b4a4abdeecce57e5c0065
AKHMAD YUSUF <new2022.akhmadyusuf@gmail.com>
2026-02-08 21:04:03 +0700

    siaga_dan_replikasi

--- bot/forms/questions/start_posreplikasi.py
+++ bot/forms/questions/start_posreplikasi.py
@@ -15,10 +15,10 @@ class StartPOSReplikasiFormQuestion(BaseFormQuestion):
             f"\n\n"
             f"Contoh:\n"
             f"<code>PC induk tidak bisa dihidupkan</code>\n"
-            f"<code>PC induk tidak bisa masuk Windows</code>\n",
-            f"<code>PC induk terkendala disk corrupt</code>\n",
-            f"<code>PC induk ganti CPU</code>\n",
-            f"<code>PC induk install ulang Windows</code>\n",
-            f"<code>PC induk maintenance EDP Lapangan</code>\n",
+            f"<code>PC induk tidak bisa masuk Windows</code>\n"
+            f"<code>PC induk terkendala disk corrupt</code>\n"
+            f"<code>PC induk ganti CPU</code>\n"
+            f"<code>PC induk install ulang Windows</code>\n"
+            f"<code>PC induk maintenance EDP Lapangan</code>\n"
             f"<code>PC induk ganti disk oleh EDP Lapangan</code>\n",
         )
